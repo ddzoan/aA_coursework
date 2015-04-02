fe240407abafb57bfcb1604af613158aa73e9345
Dan Dzoan <dandzoan@gmail.com>
2015-04-02 16:45:26 -0700

    make entries in feed show views subviews

--- app/assets/javascripts/views/feedShow.js
+++ app/assets/javascripts/views/feedShow.js
@@ -11,6 +11,11 @@ NewsReader.Views.feedShow = Backbone.View.extend({
   render: function () {
     var content = this.template({ feed: this.model });
     this.$el.html(content);
+    var entries = this.model.entries();
+    entries.each(function(entry) {
+      var newEntry = new NewsReader.Views.Entry({ model: entry });
+      this.$('.entries').append(newEntry.render().$el);
+    });
     return this;
   },
 
