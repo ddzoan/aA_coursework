1f5b6957239eebb54be426e057deb01b3593ba6f
Dan Dzoan <dandzoan@gmail.com>
2015-04-02 14:56:21 -0700

    entries

---
+++ app/assets/javascripts/collections/entries.js
@@ -0,0 +1,10 @@
+NewsReader.Collections.Entries = Backbone.Collection.extend({
+  url: function () {
+    return this.feed.url() + '/entries';
+  },
+  model: NewsReader.Models.Entry,
+
+  initialize: function (models, options) {
+    this.feed = options.feed;
+  }
+});
--- app/assets/javascripts/models/feed.js
+++ app/assets/javascripts/models/feed.js
@@ -1,3 +1,18 @@
 NewsReader.Models.Feed = Backbone.Model.extend({
-  urlRoot: 'api/feeds'
+  urlRoot: 'api/feeds',
+
+  entries: function () {
+    if (this._entries) {
+      return this._entries;
+    } else {
+      this._entries = new NewsReader.Collections.Entries([], { feed: this });
+      return this._entries;
+    }
+  },
+
+  parse: function (response) {
+    this.entries().set(response.latest_entries);
+    delete response.lastest_entries;
+    return response;
+  }
 });
