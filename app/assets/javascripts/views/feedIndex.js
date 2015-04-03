47553edca98e762d1327ee8baea6b39f25e38bdc
Dan Dzoan <dandzoan@gmail.com>
2015-04-02 17:16:30 -0700

    change feed index items to subviews and implement delete button to delete feeds

--- app/assets/javascripts/views/feedIndex.js
+++ app/assets/javascripts/views/feedIndex.js
@@ -1,14 +1,17 @@
 NewsReader.Views.feedIndex = Backbone.View.extend({
   template: JST['feed_index'],
 
-
-  initialize: function(){
+  initialize: function () {
     this.listenTo(this.collection, 'sync', this.render);
   },
 
-  render: function(){
+  render: function () {
     var content = this.template({feeds: this.collection});
     this.$el.html(content);
+    this.collection.each(function(feed) {
+      var newFeedItem = new NewsReader.Views.feedIndexItem({ model: feed });
+      this.$('.feeds').append(newFeedItem.render().$el);
+    });
     return this;
   }
 });
