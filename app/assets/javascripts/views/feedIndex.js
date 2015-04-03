2c4e8af9e5b24d72ce56b07a3b64576b3f357d87
Dan Dzoan <dandzoan@gmail.com>
2015-04-02 17:51:13 -0700

    add form to add new rss feed on the home page

--- app/assets/javascripts/views/feedIndex.js
+++ app/assets/javascripts/views/feedIndex.js
@@ -8,9 +8,11 @@ NewsReader.Views.feedIndex = Backbone.View.extend({
   render: function () {
     var content = this.template({feeds: this.collection});
     this.$el.html(content);
+    var form = new NewsReader.Views.newFeedForm({ collection: this.collection });
+    this.$(".newfeed").append(form.render().$el);
     this.collection.each(function(feed) {
       var newFeedItem = new NewsReader.Views.feedIndexItem({ model: feed });
-      this.$('.feeds').append(newFeedItem.render().$el);
+      this.$(".feeds").append(newFeedItem.render().$el);
     });
     return this;
   }
